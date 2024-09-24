5031a3b07f5dc2c4c62e4b939a4d6d30b147db08
Gregory Grant <gregorygrant@Gregorys-Mini.lan>
2024-09-24 14:18:31 -0500

    third commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,15 +10,8 @@ import { faZ, faDollar, } from '@fortawesome/free-solid-svg-icons'
 function Footer() {
   return (
     <div>
-         <div className='topfooter' style={{backgroundColor:'#001385', width:'100%', height:'100%', padding:'50px'}}>
-<div className='topFooterItems' style={{
-  width:'100%',
-  height:'100%',
-  display:'flex',
-  flexWrap: 'wrap' ,
-  justifyContent: 'center',
-  alignItems: 'center',
-}}>
+         <div className='topfooter'>
+<div className='topFooterItems'>
   
 <div style={{  padding:'20px', margin:'20px'}}>
     <h3 style={{fontSize:'20px', marginBottom:'10px'}}>A Ray of Hope Outreach Ministries</h3>
@@ -48,7 +41,7 @@ function Footer() {
             </div>
 
             <div style={{ margin:'20px', padding:'20px'}}>
-                <h3 style={{fontSize:'20px', marginBottom:'35px'}}>Danate</h3>
+                <h3 style={{fontSize:'20px', marginBottom:'35px'}}>Ways To Danate</h3>
                 <p  style={{color:'var(--white', marginBottom:'20px'}}>You Donation of any amount is greatly apreciated</p>
                 <div style={{flexDirection:'column', display:'block'}}>
                 <Link to="/donate"><button style={{margin:'20px'}}>PayPal <FontAwesomeIcon style={{backgroundColor:'white', padding:'3px', width:"18PX", borderRadius:'50%', color:'#0790d0'}} icon={faPaypal} /></button></Link>
