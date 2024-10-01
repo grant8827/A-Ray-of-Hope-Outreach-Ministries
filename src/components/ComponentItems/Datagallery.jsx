bae84be8ccef86949d2e50a6346e74fb4d09b222
grant8827 <grant8827@yahoo.com>
2024-10-01 16:33:50 -0500

    error fixed 2

--- src/components/ComponentItems/Datagallery.jsx
+++ src/components/ComponentItems/Datagallery.jsx
@@ -4,7 +4,7 @@ function Datagallery(props) {
   return (
     <div>
       <div style={{margin:'20px'}}>
-        <img style={{width:'300px'}} src={props.imagedata} />
+        <img style={{width:'300px'}} src={props.imagedata} alt='galleryimg'/>
       <p>{props.gallerytext}</p>
       </div>
      
